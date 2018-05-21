ebf94515da84f367a8deb57921abfaddcc535d89
unknown <hrbkcwl@163.com>
2018-05-21 19:04:22 +0800

    112

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,9 @@ hello('Hello World');
 var height = document.getElementById("logo_h").offsetHeight;
 document.getElementsByClassName("nav_b")[0].style.height = height + 'px';
 document.getElementsByClassName("yd_b")[0].style.height = height + 'px';
+
+
+$(".video_fm").click(function(){
+        $(".video_fm").hide();
+        $('video').get(0).play();
+    });
