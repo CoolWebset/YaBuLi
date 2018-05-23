ab41866eb3981a66951775c1bfe07d55805ef8fd
wzs28150 <wz666s@163.com>
2018-05-23 18:27:17 +0800

    add

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,11 @@
 import 'object-fit-images';
 import 'picturefill';
-import './svg-sprites';
-import hello from './hello-world';
 import './bootstrap.min';
 import '../css/bootstrap.css';
-hello('Hello World');
-var height = document.getElementById("logo_h").offsetHeight;
-document.getElementsByClassName("nav_b")[0].style.height = height + 'px';
-document.getElementsByClassName("yd_b")[0].style.height = height + 'px';
+
 
 
 $(".video_fm").click(function(){
-        $(".video_fm").hide();
-        $('video').get(0).play();
-    });
+  $(".video_fm").hide();
+  $('video').get(0).play();
+});
