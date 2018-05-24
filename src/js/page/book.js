b451d1f82b6a3b494997bbf6d0cb3c2421800cc6
wzs28150 <wz666s@163.com>
2018-05-24 16:14:26 +0800

    add an

--- src/js/page/book.js
+++ src/js/page/book.js
@@ -2,6 +2,18 @@ import debug from '../package/debug/debug'
 import fdatepicker from '../package/datepicker/js/foundation-datepicker'
 import '../package/datepicker/js/locales/foundation-datepicker.zh-CN'
 
+const formChange = () =>{
+  $('.nav-tabs.nav-justified>li').click(function(){
+    const i = $(this).index();
+    $('.nav-tabs.nav-justified>li').removeClass('active');
+    $(this).addClass('active');
+    $('.book-tab').removeClass('active');
+    $('.book-tab').eq(i).addClass('active');
+    $('.book .bg .bg').removeClass('active');
+    $('.book .bg .bg').eq(i).addClass('active');
+  })
+}
+
 const timeselect = () => {
   var nowTemp = new Date();
 		var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
@@ -12,13 +24,13 @@ const timeselect = () => {
       format: 'yyyy-mm-dd hh:ii',
 			pickTime: true
 		}).on('changeDate', function (ev) {
-			// if (ev.date.valueOf() > checkout.date.valueOf()) {
-			// 	var newDate = new Date(ev.date)
-			// 	newDate.setDate(newDate.getDate() + 1);
-			// 	checkout.update(newDate);
-			// }
-			// checkin.hide();
-			// $('#outtime')[0].focus();
+			if (ev.date.valueOf() > checkout.date.valueOf()) {
+				var newDate = new Date(ev.date)
+				newDate.setDate(newDate.getDate() + 1);
+				checkout.update(newDate);
+			}
+			checkin.hide();
+			$('#outtime')[0].focus();
 		}).data('datepicker');
 		var checkout = $('#outtime').fdatepicker({
 			onRender: function (date) {
@@ -36,6 +48,7 @@ const init = (callback) => {
   callback(2)
   debug('book is load');
   timeselect();
+  formChange();
 };
 
 const book = {
