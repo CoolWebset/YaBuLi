25f949a34364dd05eb6d1bdec98c4141271ce8f9
JinXin888 <hrbkcwl@163.com>
2018-05-24 14:57:24 +0800

    add

--- src/js/page/activity.js
+++ src/js/page/activity.js
@@ -1,41 +1,22 @@
 import debug from '../package/debug/debug'
-import fdatepicker from '../package/datepicker/js/foundation-datepicker'
-import '../package/datepicker/js/locales/foundation-datepicker.zh-CN'
+import Swiper from 'swiper';
 
-const timeselect = () => {
-  var nowTemp = new Date();
-		var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
-		var checkin = $('#intime').fdatepicker({
-			onRender: function (date) {
-				return date.valueOf() < now.valueOf() ? 'disabled' : '';
-			}
-		}).on('changeDate', function (ev) {
-			if (ev.date.valueOf() > checkout.date.valueOf()) {
-				var newDate = new Date(ev.date)
-				newDate.setDate(newDate.getDate() + 1);
-				checkout.update(newDate);
-			}
-			checkin.hide();
-			$('#outtime')[0].focus();
-		}).data('datepicker');
-		var checkout = $('#outtime').fdatepicker({
-			onRender: function (date) {
-				return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
-			}
-		}).on('changeDate', function (ev) {
-			checkout.hide();
-		}).data('datepicker');
 
+const banner = () => {
+  var bannerSwiper = new Swiper('.i_ban.swiper-container', {
+    pagination: {
+      el: '.i_ban .swiper-pagination',
+    },
+    loop: true,
+  });
 }
-
 const init = (callback) => {
-  callback(2)
-  debug('book is load');
-  timeselect();
+  callback(1)
+  debug('activity is load');
+  banner();
 };
-
-const book = {
+const activity = {
   init: init
 };
 
-export default book;
+export default activity;
