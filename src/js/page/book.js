cc618205ffb79684eb243b7d3011d21b3631af9c
wzs28150 <wz666s@163.com>
2018-05-24 14:22:54 +0800

    add timepacker

--- src/js/page/book.js
+++ src/js/page/book.js
@@ -1,9 +1,37 @@
 import debug from '../package/debug/debug'
+import fdatepicker from '../package/datepicker/js/foundation-datepicker'
+import '../package/datepicker/js/locales/foundation-datepicker.zh-CN'
 
+const timeselect = () => {
+  var nowTemp = new Date();
+		var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+		var checkin = $('#intime').fdatepicker({
+			onRender: function (date) {
+				return date.valueOf() < now.valueOf() ? 'disabled' : '';
+			}
+		}).on('changeDate', function (ev) {
+			if (ev.date.valueOf() > checkout.date.valueOf()) {
+				var newDate = new Date(ev.date)
+				newDate.setDate(newDate.getDate() + 1);
+				checkout.update(newDate);
+			}
+			checkin.hide();
+			$('#outtime')[0].focus();
+		}).data('datepicker');
+		var checkout = $('#outtime').fdatepicker({
+			onRender: function (date) {
+				return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+			}
+		}).on('changeDate', function (ev) {
+			checkout.hide();
+		}).data('datepicker');
+
+}
 
 const init = (callback) => {
   callback(2)
   debug('book is load');
+  timeselect();
 };
 
 const book = {
