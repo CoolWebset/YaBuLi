8ec2a1a1a85e6aaeccc77562c24bc0839e52b757
wzs28150 <wz666s@163.com>
2018-05-25 10:27:37 +0800

    add picture

--- src/js/page/picture.js
+++ src/js/page/picture.js
@@ -1,8 +1,14 @@
 import debug from '../package/debug/debug'
 
+const picClick = () => {
+  $('.picture .item img').click(function(){
+    alert(1)
+  });
+}
 const init = (callback) => {
   callback(1)
   debug('picture is load');
+  picClick();
 };
 const picture = {
   init: init
