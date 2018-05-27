f01a12d5d06503b5fd357feedfd2b41771ec6a3f
JinXin888 <31975097+JinXin888@users.noreply.github.com>
2018-05-28 00:37:54 +0800

    add

--- src/js/page/activity.js
+++ src/js/page/activity.js
@@ -8,7 +8,39 @@ const banner = () => {
     loop: true,
   });
 }
+const hd_three = () => {
+  var bannerSwiper = new Swiper('.swiper-hd_three', {
+    pagination: {
+      el: '.swiper-hd_three .swiper-pagination',
+    },
+    loop: true,
+    slidesPerView: 3,
+    paginationClickable: true,
+    spaceBetween: 30
+  });
+}
+const videoCtrl = () => {
+  $(".video_fm").click(function() {
+    $(".video_fm").addClass('hide');
+    $('video').trigger("play");
+    $('video').removeClass('pause');
+    $('video').addClass('play');
+  });
+  $("video").addClass('pause'); //for check pause or play add a class
+  $('video').click(function() {
+    if ($(this).hasClass('pause')) {
+
+    } else {
+      $("video").trigger("pause");
+      $(this).removeClass('play');
+      $(".video_fm").removeClass('hide');
+      $(this).addClass('pause');
+    }
+  })
+}
 const init = (callback) => {
+  hd_three();
+  videoCtrl();
   callback(1)
   debug('activity is load');
   banner();
