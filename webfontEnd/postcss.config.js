46906e91c21ff66e09839cdfd107b6d24798c31f
wzs28150 <wz666s@163.com>
2018-06-06 18:05:23 +0800

    add

--- webfontEnd/postcss.config.js
+++ webfontEnd/postcss.config.js
@@ -4,6 +4,6 @@ module.exports = {
     'postcss-import': {},
     'postcss-url': {},
     // to edit target browsers: use "browserslist" field in package.json
-    autoprefixer: { }
+    autoprefixer: {}
   }
 };
